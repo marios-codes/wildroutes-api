4ce1fdb6b237ca395391c53ec8730f98d24aa88a
Marios Zoutis <marioszou@proton.me>
2026-04-27 14:56:50 +0300

    Create initial AppError global middleware implementation

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { AppError } from '../utils/app-error';
 
 export const appErrorHandler = (
   error: Error,
@@ -6,6 +7,12 @@ export const appErrorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
   return res.status(500).json({
     success: false,
     message: 'Internal server error',
